b92aea48f31714aad53c30b25a34c10c46332ca8
Ghozti <ybalderas1005@gmail.com>
2021-05-12 12:32:23 -0400

    - filled in Screen and Game - added the background in the assets folder

--- core/src/ghozti/game/Game.java
+++ core/src/ghozti/game/Game.java
@@ -2,8 +2,28 @@ package ghozti.game;
 
 public class Game extends com.badlogic.gdx.Game {
 
+	Screen screen;
+
 	@Override
 	public void create() {
 		//The purpose of this class is to just get things started, all the work will be done in other classes.
+		screen = new Screen();
+		setScreen(screen);//will set this as the game screen
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		screen.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		screen.resize(width, height);
 	}
 }
--- core/src/ghozti/game/Screen.java
+++ core/src/ghozti/game/Screen.java
@@ -1,6 +1,25 @@
 package ghozti.game;
 
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
 public class Screen implements com.badlogic.gdx.Screen {
+
+    //Screen
+    private Camera camera;
+    private Viewport viewport;
+    //Game
+    private SpriteBatch batch;
+    private Texture background;
+    //Timing
+    private int backgroundOffset;
+    //World
+    private final int WORLD_WIDTH = 72;
+    private final int WORLD_HEIGHT = 128;
+
+
     @Override
     public void show() {
 
