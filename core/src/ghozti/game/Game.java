85488e035f8d61fedbfe2f833098a61728cefe7b
Ghozti <ybalderas1005@gmail.com>
2021-05-12 11:59:04 -0400

    - added Screen.java

--- core/src/ghozti/game/Game.java
+++ core/src/ghozti/game/Game.java
@@ -1,33 +1,9 @@
 package ghozti.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class Game extends com.badlogic.gdx.Game {
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		//The purpose of this class is to just get things started, all the work will be done in other classes.
 	}
 }
---
+++ core/src/ghozti/game/Screen.java
@@ -0,0 +1,38 @@
+package ghozti.game;
+
+public class Screen implements com.badlogic.gdx.Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
